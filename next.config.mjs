7362610de4d497a5aafa93fba6c2c8971a2a1139
Jorge Antonio Paisano Herrera <SAFDGSAC034@sfp-ingresos.morelos.gob.mx>
2026-07-07 13:54:35 -0600

    comentar ruta base

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  
+  //basePath: process.env.NODE_ENV === 'production' ? '/portal-planeacion' : '',
+  //assetPrefix: process.env.NODE_ENV === 'production' ? '/portal-planeacion' : '',
   trailingSlash: true,
 
   typescript: {
